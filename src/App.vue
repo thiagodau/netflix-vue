440b56e8f9e6f83d29b5bfdb8792a475c9ff97b2
Thiago Dau <thiagoorodrigues12@gmail.com>
2021-02-09 22:58:32 -0300

    Editando a estrutura do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h2>Meu componente! Projeto: {{ nomeProjeto }} </h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+
+//import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      nomeProjeto: 'Netflix with Vue.js'
+    }
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
